609840bd8296beb2ab523870fe7e999c3719ee03
BHANUCHARAN888 <bhanucharankalla8523@gmail.com>
2025-07-12 12:00:28 +0530

    Added 01_variables.py for Python brush-up

---
+++ 01_variables.py
@@ -0,0 +1,26 @@
+# python is case sensitive language and follows Indentation
+# basic variable
+x=20
+y=40
+z=x+y
+print(z)
+# string variable
+name="BHANU"
+age=18
+print("my name is",name,"and my age is",age)
+#python local variable
+def sum(x,y):
+    sum=x+y
+    return sum
+print(sum(10,20))
+# python in global variable
+x=10
+y=20
+def sum(x,y):
+    sum=x+y
+    return sum
+print(sum(x,y))
+# small idea about functions
+def function_name():
+    print("hello there i am learning python")
+function_name()
