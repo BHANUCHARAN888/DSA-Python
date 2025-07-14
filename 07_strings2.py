f4ac217396f01741652b6f1a674b6ade9072b1b3
BHANUCHARAN888 <bhanucharankalla8523@gmail.com>
2025-07-14 10:06:07 +0530

    Added some more string methods with an example

---
+++ 07_strings2.py
@@ -0,0 +1,32 @@
+# some string methods
+a=["red", "green", "blue"]
+print(a)
+# append 
+a.append("yellow")
+print(a)
+# insert
+a.insert(2,"pink")
+print(a)
+# remove
+a.remove("green")
+print(a)
+# pop
+a.pop(1)
+print(a)
+# sort
+a.sort()
+print(a)
+# reverse
+a.reverse()
+print(a)
+# clear
+a.clear()
+print(a)
+#index
+a=["red", "green", "blue"]
+print(a.index("green"))
+# count
+print(a.count("red"))
+# join
+b = " ".join(a)
+print(b)
